5a982339a5d63406a71a22282dd42d7eac7216cb
Romensondev <romensonfelixdonascimento@gmail.com>
2024-09-20 10:32:09 -0300

    changed readme.md

---
+++ README.m
@@ -0,0 +1,6 @@
+# Check original documentation
+## Kubernetes
+https://cert-manager.io/docs/installation/
+https://cert-manager.io/docs/configuration/acme/dns01/cloudflare/
+## Cloudflare
+https://developers.cloudflare.com/fundamentals/api/get-started/create-token/
